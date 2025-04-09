4e53555c0d62edbeb80d5f7bb4c6478fc4f3f473
Anian Schleyer <anian.schleyer@tum.de>
2025-04-09 09:41:02 +0200

    Update packages

--- ArtemisExamCheckKit/Package.swift
+++ ArtemisExamCheckKit/Package.swift
@@ -16,7 +16,7 @@ let package = Package(
     ],
     dependencies: [
         .package(url: "https://github.com/daltoniam/Starscream", exact: "4.0.4"),
-        .package(url: "https://github.com/ls1intum/artemis-ios-core-modules", from: "15.6.2")
+        .package(url: "https://github.com/ls1intum/artemis-ios-core-modules", from: "16.1.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
